03d6d482b956efdf956cd5e9056503342872e526
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-06-05 10:54:44 +0800

    fix: fix assets config

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('zkey');
+config.resolver.assetExts.push('bin');
+config.resolver.assetExts.push('json');
+config.resolver.assetExts.push('local');
 
 module.exports = config;
